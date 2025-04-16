e31f4dd04073df25f932307e1e0ab710e14dedaf
MakSimKlim <83maxomus@mail.ru>
2025-04-16 16:23:01 +0500

    commit from VS

--- Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7053c4b79b740f5fb56b4e4df6e7cb6c59ccbd0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Multitenant/obj/Debug/net8.0/Multitenant.AssemblyInfo.cs
+++ Multitenant/obj/Debug/net8.0/Multitenant.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Multitenant")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7053c4b79b740f5fb56b4e4df6e7cb6c59ccbd0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Multitenant")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Multitenant")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
