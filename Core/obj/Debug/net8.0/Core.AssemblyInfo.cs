f9c2dbfe923b76b7c43ba1c2ffb79d2ddaf2a6fe
MakSimKlim <83maxomus@mail.ru>
2025-04-16 17:36:22 +0500

    Product Service DONE

--- Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
+++ Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c257c13c5c4c4943c987a842e28daabf41d5cfc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bb7cb43f2a2ce47c2007292ba59b0d1cfd54050")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
